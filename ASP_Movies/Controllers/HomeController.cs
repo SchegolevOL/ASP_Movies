e52821117dfb567246b87ebd789d26b674d9d5cb
Oleg Schegol <olschegolev@yahoo.com>
2023-03-21 12:42:59 +0500

    add public HomeController(IMovieApiService movieApiService)

--- ASP_Movies/Controllers/HomeController.cs
+++ ASP_Movies/Controllers/HomeController.cs
@@ -8,10 +8,16 @@ namespace ASP_Movies.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IMovieApiService _movieApiService;
 
-		public HomeController(ILogger<HomeController> logger)
+		//public HomeController(ILogger<HomeController> logger)
+		//{
+		//	_logger = logger;
+		//}
+
+		public HomeController(IMovieApiService movieApiService)
 		{
-			_logger = logger;
+			this._movieApiService = movieApiService;
 		}
 
 		public IActionResult Index()
@@ -20,10 +26,10 @@ namespace ASP_Movies.Controllers
 		}
 		public async Task<IActionResult> Search(string title)
 		{
-			MovieApiService movieApiService = new MovieApiService();
+			
 			
 			ViewBag.MovieTitle = title;
-			ViewBag.Result = await movieApiService.SearchByTitleAsync(title);
+			ViewBag.Result = await _movieApiService.SearchByTitleAsync(title);
 
 
 			return View();
--- ASP_Movies/Program.cs
+++ ASP_Movies/Program.cs
@@ -4,10 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//builder.Services.AddScoped<MovieApiService>();//создается новый обект на каждое обращение
+
+builder.Services.AddScoped<IMovieApiService, MovieApiService>();//создается новый обект на каждое обращение
 //builder.Services.AddTransient<MovieApiService>();//при обращении создается один обект при выходе из контроллера уничтожается
 builder.Services.AddSingleton<MovieApiService>();//обращение к классу один на весь проек все обращения идут к нему после использования уничтожается
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
