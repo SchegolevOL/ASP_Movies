7c822815087764aa5ed4a949ad846e49be590bd4
Oleg Schegol <olschegolev@yahoo.com>
2023-03-21 16:55:21 +0500

    mod class MovieApiService

--- ASP_Movies/Services/MovieApiService.cs
+++ ASP_Movies/Services/MovieApiService.cs
@@ -9,21 +9,22 @@ namespace ASP_Movies.Services
 
 	public class MovieApiService : IMovieApiService
 	{
-		public string BaseUrl { get; set; }
-		public string ApiKey { get; set; }
-
+		//public string BaseUrl { get; set; }
+		//public string ApiKey { get; set; }
+		public MovieApiOptions movieApiOptions { get; set; }
 		public  HttpClient httpClient { get; set; }
 
 		public MovieApiService(IHttpClientFactory httpClientFactory, IOptions<MovieApiOptions>options)
 		{
-			BaseUrl = options.Value.BaseUrl;
-			ApiKey = options.Value.ApiKey;
+			//BaseUrl = options.Value.BaseUrl;
+			//ApiKey = options.Value.ApiKey;
+			movieApiOptions = options.Value;
 			httpClient = httpClientFactory.CreateClient();
 		}
 
 		public async Task<MovieApiResponse> SearchByTitleAsync(string title)
 		{
-			var response = await httpClient.GetAsync($"{BaseUrl}?s={title}%20man&apikey={ApiKey}");
+			var response = await httpClient.GetAsync($"{movieApiOptions.BaseUrl}?s={title}%20man&apikey={movieApiOptions.ApiKey}");
 			var result = await response.Content.ReadFromJsonAsync<MovieApiResponse>();//????????????
 			
 			return result;
